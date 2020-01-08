1a99b095caed9035109311dae818537d22f22325
jmusau62-gmail <jmusau62@gmail.com>
2020-01-08 06:13:46 +0300

    added functioality to class hero

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,20 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
-}
+    private String heroName;
+    private static ArrayList<Hero>allHero= new ArrayList<>();
+    private int age;
+    private ArrayList<Squad>heroSquad;
+    public Hero(String heroName){
+        this.heroName=heroName;
+        allHero.add(this);
+        this.age = allHero.size();
+    this.heroSquad=new ArrayList<Squad>();}
+    public String getHeroName() {return heroName;}
+   public int getAge() {return age;}
+public static Hero findHero(int age) {return allHero.get(age-1);}
+public void addSquadToHero(Squad newSquad){heroSquad.add(newSquad);}
+public ArrayList<Squad>getAllThisHeroSquads(){return heroSquad;}
+public static ArrayList<Hero> getAllHeroes(){return allHero;}}
